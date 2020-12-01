2f0de1e6e9e9dd00c4984a1ee2a93a15f7f2b95e
unknown <moustikitos@gmail.com>
2020-12-01 22:56:59 +0100

    markup language use pypi

--- setup.py
+++ setup.py
@@ -1,39 +1,40 @@
 # -*- coding:utf-8 -*-
 try:
-	from setuptools import setup
-	import wheel
+    from setuptools import setup
+    import wheel
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 kw = {}
 
 f = open("VERSION", "r")
 long_description = open("rst/pypi.rst", "r")
 kw.update(**{
-	"version": f.read().strip(),
-	"name": "Tyf",
-	"keywords": ["ifd", "tiff", "jpeg", "exif", "gps", "geotiff", "PIL", "Pillow"],
-	"author": "Bruno THOORENS",
-	"author_email": "bruno.thoorens@free.fr",
-	"maintainer": "Bruno THOORENS",
-	"maintainer_email": "bruno.thoorens@free.fr",
-	"url": "http://bruno.thoorens.free.fr",
-	"download_url": "https://github.com/Moustikitos/tyf",
-	"description": "Pythonic way to read and edit IFD and EXIF tags.",
-	"long_description": long_description.read(),
-	"packages": ["Tyf"],
-	"license": "Copyright 2015-2016, THOORENS Bruno, BSD licence",
-	"classifiers": [
-		'Development Status :: 6 - Mature',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Information Technology',
-		'License :: OSI Approved :: BSD License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 3',
-		'Topic :: Multimedia :: Graphics',
-	],
+    "version": f.read().strip(),
+    "name": "Tyf",
+    "keywords": ["ifd", "tiff", "jpeg", "exif", "gps", "geotiff", "PIL", "Pillow"],
+    "author": "Bruno THOORENS",
+    "author_email": "bruno.thoorens@free.fr",
+    "maintainer": "Bruno THOORENS",
+    "maintainer_email": "bruno.thoorens@free.fr",
+    "url": "http://bruno.thoorens.free.fr",
+    "download_url": "https://github.com/Moustikitos/tyf",
+    "description": "Pythonic way to read and edit IFD and EXIF tags.",
+    "long_description": long_description.read(),
+    "long_description_content_type": "text/markdown",
+    "packages": ["Tyf"],
+    "license": "Copyright 2015-2016, THOORENS Bruno, BSD licence",
+    "classifiers": [
+        'Development Status :: 6 - Mature',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics',
+    ],
 })
 long_description.close()
 f.close()
